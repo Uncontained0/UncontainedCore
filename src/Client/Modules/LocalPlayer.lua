f61a051675c1051c5138c43f784b85fb876a0dce
Uncontained0 <uncontained0@gmail.com>
2022-02-28 21:47:00 -0600

    added character data

--- src/Client/Modules/LocalPlayer.lua
+++ src/Client/Modules/LocalPlayer.lua
@@ -1,4 +1,5 @@
 local Event = require(script.Parent.ClientNet)
+local Signal = require(game.ReplicatedStorage.Utility.Signal)
 
 local PermissionsEvent = Event.new("LocalPermissions")
 
@@ -8,5 +9,22 @@ function LocalPlayer.HasPermission (Permission:string): Promise
     local Promise = PermissionsEvent:Call (Permission)
     return Promise
 end
+
+LocalPlayer.HealthChanged = Signal.new ()
+LocalPlayer.CharacterAdded = Signal.new ()
+LocalPlayer.Died = Signal.new ()
+
+game.Players.LocalPlayer.CharacterAdded:Connect(function(Character:Model)
+    local Humanoid = Character:WaitForChild("Humanoid")
+    LocalPlayer.Humanoid = Humanoid
+    LocalPlayer.Character = Character
+    LocalPlayer.CharacterAdded:Fire (Character)
+    Humanoid.HealthChanged:Connect(function()
+        LocalPlayer.HealthChanged:Fire (Humanoid.Health)
+    end)
+    Humanoid.Died:Connect(function()
+        LocalPlayer.Died:Fire ()
+    end)
+end)
 
 return LocalPlayer
